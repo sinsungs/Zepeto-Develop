90f6bf0f6f48e4f0c83489b8998fddb58268bf7b
sinsungs <39232875+sinsungs@users.noreply.github.com>
2023-04-27 20:26:41 +0900

    Add files via upload

---
+++ NpcRequest.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
+
+    export default class NpcRequest {
+              
+    Start() {
+      console.log('log : Hello World')
+    }
+    // RequestUserId(zepetoUserId: string) {
+    //     const url = "http://localhost:8080/api/zepeto";
+    //     const headers = {
+    //       'Content-Type': 'application/json',
+    //     };
+    //     const body = {
+    //       "name": zepetoUserId,
+    //       "challenge": "miracle morning",
+    //     };
+      
+    //     axios.post(url, body, { headers })
+    //       .then((res: AxiosResponse) => {
+    //         console.log(`HTTP Result: ${JSON.stringify(res.data)}`);
+    //       })
+    //       .catch((error) => {
+    //         console.log(`HTTP Error: ${error}`);
+    //       });
+          
+  
+    //     }
+        
+        }
